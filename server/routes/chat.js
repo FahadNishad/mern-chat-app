4a7c23afacbb15b60bcdda53175bcf3fbe1536bf
FahadNishad <fahadnishad1122@gmail.com>
2024-09-08 19:18:13 +0500

    update

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { newGroupChat } from "../controllers/chat.js";
+import { addMembers, getMyChats, getMyGroups, newGroupChat } from "../controllers/chat.js";
 
 const app = express.Router();
 
@@ -8,5 +8,7 @@ app.use(isAuthenticated);
 
 
 app.post("/new", newGroupChat)
-
+app.get("/my", getMyChats)
+app.get("/my/groups", getMyGroups)
+app.put("/addmembers", addMembers);
 export default app;
