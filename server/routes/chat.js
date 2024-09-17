58b04ca41d1d8078178b7ed967172501ffc96e57
FahadNishad <fahadnishad1122@gmail.com>
2024-09-17 21:54:33 +0500

    update

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -14,32 +14,49 @@ import {
   sendAttachments,
 } from "../controllers/chat.js";
 import { attachmentsMulter } from "../middlewares/multer.js";
+import {
+  addMemberValidator,
+  chatIdValidator,
+  newGroupValidator,
+  removeMemberValidator,
+  renameValidator,
+  sendAttachmentsValidator,
+  validateHandler,
+} from "../lib/validators.js";
 
 const app = express.Router();
 
 app.use(isAuthenticated);
 
-app.post("/new", newGroupChat);
+app.post("/new", newGroupValidator, validateHandler, newGroupChat);
 app.get("/my", getMyChats);
 app.get("/my/groups", getMyGroups);
-app.put("/addmembers", addMembers);
-app.put("/removemember", removeMember);
-app.delete("/leave/:id", leaveGroup);
+app.put("/addmembers", addMemberValidator(), validateHandler, addMembers);
+app.put(
+  "/removemember",
+  removeMemberValidator(),
+  validateHandler,
+  removeMember
+);
+app.delete("/leave/:id", chatIdValidator(), validateHandler, leaveGroup);
 
 // Send Attachments
 app.post(
   "/message",
   attachmentsMulter,
-  // sendAttachmentsValidator(),
-  // validateHandler,
+  sendAttachmentsValidator(),
+  validateHandler,
   sendAttachments
 );
 
 // Get Messages
-app.get("/message/:id", getMessages);
+app.get("/message/:id", chatIdValidator(), validateHandler, getMessages);
 
 // Get Chat Details, rename,delete
-app.route("/:id").get(getChatDetails).put(renameGroup).delete(deleteChat);
-
+app
+  .route("/:id")
+  .get(chatIdValidator(), validateHandler, getChatDetails)
+  .put(renameValidator(), validateHandler, renameGroup)
+  .delete(chatIdValidator(), validateHandler, deleteChat);
 
 export default app;
